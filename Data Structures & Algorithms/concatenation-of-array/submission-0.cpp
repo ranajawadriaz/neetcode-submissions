e34ff593b39293259941808bbdfd2e5f1b13a33f
Rana Jawad Riaz <141062578+ranajawadriaz@users.noreply.github.com>
2026-07-30 20:31:11 +0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans(nums.size()*2);
+
+         for(int i=0;i<nums.size();i++)
+        {
+            ans[i]=nums[i];
+            ans[i+nums.size()]=nums[i];
+        }
+
+
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     ans.push_back(nums[i]);
+        // }
+
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     ans.push_back(nums[i]);
+        // }
+
+        return ans;
+        
+    }
+};
